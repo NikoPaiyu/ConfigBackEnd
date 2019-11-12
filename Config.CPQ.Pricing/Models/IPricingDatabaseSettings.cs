55ac67ce2a271fa8b159940513ee97b53ab04b52
Kruthi-Siddesh <57387908+Kruthi-Siddesh@users.noreply.github.com>
2019-11-12 17:09:16 +0530

    Initial code
    Initital code

---
+++ Config.CPQ.Pricing/Models/IPricingDatabaseSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Config.CPQ.Pricing.Models
+{
+       public interface IPricingDatabaseSettings
+    {
+       
+            string PricingCollectionName { get; set; }
+            string ConnectionString { get; set; }
+            string DatabaseName { get; set; }
+        
+    }
+}
---
+++ Config.CPQ.Stat/Controllers/StatController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using Config.CPQ.Static.Models;
+
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Config.CPQ.Static.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatController : ControllerBase
+    {
+        private readonly StatServices _stat1Services;
+
+        public StatController(StatServices stat1Services)
+        {
+            _stat1Services = stat1Services;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Stat1>> Get() =>
+            _stat1Services.Get();
+
+        [HttpGet("{id:length(24)}", Name = "GetStat1")]
+        public ActionResult<Stat1> Get(string id)
+        {
+            var stat1 = _stat1Services.Get(id);
+
+            if (stat1 == null)
+            {
+                return NotFound();
+            }
+
+            return stat1;
+        }
+
+        [HttpPost]
+        public ActionResult<Stat1> Create(Stat1 stat1)
+        {
+            _stat1Services.Create(stat1);
+
+            return CreatedAtRoute("GetQuote", new { id = stat1.Id.ToString() }, stat1);
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public IActionResult Update(string id, Stat1 stat1In)
+        {
+            var stat1 = _stat1Services.Get(id);
+
+            if (stat1 == null)
+            {
+                return NotFound();
+            }
+
+            _stat1Services.Update(id, stat1In);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public IActionResult Delete(string id)
+        {
+            var stat1 = _stat1Services.Get(id);
+
+            if (stat1 == null)
+            {
+                return NotFound();
+            }
+
+            _stat1Services.Remove(stat1.Id);
+
+            return NoContent();
+        }
+    }
+}
