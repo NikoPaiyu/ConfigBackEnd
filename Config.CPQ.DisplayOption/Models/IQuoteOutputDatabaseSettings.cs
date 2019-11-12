48fcc441d7b4e8613c9e0fdfa744b55aafb2cd5d
ChandanaG <57387834+chandana2297@users.noreply.github.com>
2019-11-12 15:54:53 +0530

    Inital Code
    Inital Code

---
+++ Config.CPQ.DisplayOption/Models/IQuoteOutputDatabaseSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Config.CPQ.QuoteOutput.Models
+{
+    public interface IQuoteOutputDatabaseSettings
+    {
+        string QuoteOutputCollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+}
