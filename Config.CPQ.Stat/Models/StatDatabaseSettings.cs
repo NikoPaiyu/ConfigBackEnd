2838e302d7008d67c0dc6af0331120132106d978
Kruthi-Siddesh <57387908+Kruthi-Siddesh@users.noreply.github.com>
2019-11-13 15:18:13 +0530

    Separate Solution
    Separate Solution

---
+++ Config.CPQ.Stat/Models/StatDatabaseSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Config.CPQ.Static.Models
+{
+    public class StatDatabaseSettings : IStatDatabaseSettings
+    {
+        public string StatCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
---
+++ Config.CPQ.Stat/Services/StatServices.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Config.CPQ.Static.Models;
+using MongoDB.Driver;
+
+
+namespace Config.CPQ.Static
+{
+    public class StatServices
+    {
+        private readonly IMongoCollection<Stat1> _stat1settings;
+        public StatServices(IStatDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _stat1settings = database.GetCollection<Stat1>(settings.StatCollectionName);
+        }
+
+        public List<Stat1> Get() =>
+        _stat1settings.Find(stat1 => true).ToList();
+
+        public Stat1 Get(string id) =>
+        _stat1settings.Find<Stat1>(stat1 => stat1.Id == id).FirstOrDefault();
+
+        public Stat1 Create(Stat1 stat1)
+        {
+            _stat1settings.InsertOne(stat1);
+            return stat1;
+        }
+
+        public void Update(string id, Stat1 stat1In) =>
+            _stat1settings.ReplaceOne(stat1 => stat1.Id == id, stat1In);
+
+        public void Remove(Stat1 stat1In) =>
+            _stat1settings.DeleteOne(stat1 => stat1.Id == stat1In.Id);
+
+        public void Remove(string id) =>
+            _stat1settings.DeleteOne(stat1 => stat1.Id == id);
+
+    }
+}
