0b05182a354c0130365404c7a764f8554e72cd66
Kruthi-Siddesh <57387908+Kruthi-Siddesh@users.noreply.github.com>
2019-11-13 15:14:07 +0530

    Separate Solution

---
+++ Config.CPQ.QuoteSettings/Models/QuoteDatabaseSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace Config.CPQ.QuoteSettings.Models
+{
+    public class QuoteDatabaseSettings : IQuoteDatabaseSettings
+    {
+        public string QuoteCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+
+}
---
+++ Config.CPQ.QuoteSettings/Startup.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using Config.CPQ.QuoteSettings.Models;
+using Config.CPQ.QuoteSettings.Services;
+
+namespace Config.CPQ.QuoteSettings
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // requires using Microsoft.Extensions.Options
+            services.Configure<QuoteDatabaseSettings>(
+                Configuration.GetSection(nameof(QuoteDatabaseSettings)));
+
+            services.AddSingleton<IQuoteDatabaseSettings>(sp =>
+                sp.GetRequiredService<IOptions<QuoteDatabaseSettings>>().Value);
+
+            services.AddSingleton<QuoteServices>();
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
