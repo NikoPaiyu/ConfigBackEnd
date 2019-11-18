6424854d7f9e76144e2248d9609127d6cd6d1107
ChandanaG <57387834+chandana2297@users.noreply.github.com>
2019-11-18 13:11:40 +0530

    quoteout

--- Config.CPQ.DisplayOption/Models/QuoteOutputDatabaseSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Config.CPQ.QuoteOutput.Models
-{
-    public class QuoteOutputDatabaseSettings:IQuoteOutputDatabaseSettings
-    {
-        public string QuoteOutputCollectionName { get; set; }
-        public string ConnectionString { get; set; }
-        public string DatabaseName { get; set; }
-    }
-}
