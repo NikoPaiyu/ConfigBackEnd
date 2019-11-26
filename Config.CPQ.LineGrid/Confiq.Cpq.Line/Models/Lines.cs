f38b896cc31a963b2e1b80a1659e11b6539d313e
ChandanaG <57387834+chandana2297@users.noreply.github.com>
2019-11-26 18:06:26 +0530

    LineAPI

---
+++ Config.CPQ.LineGrid/Confiq.Cpq.Line/Models/Lines.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Confiq.Cpq.Line.Models
+{
+    public class Lines
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string quoteNumber { get; set; }
+        public int Quantity { get; set; }
+        public string productID { get; set; }
+        public string Description { get; set; } 
+        public double listPrice { get; set; }
+        public double stdDisc { get; set; }
+        public double stdNetprice { get; set; }
+        public double restDisc { get; set; }
+        public float restType { get; set; }
+        public double reqtNet { get; set; }
+}
+
+
+}
+
