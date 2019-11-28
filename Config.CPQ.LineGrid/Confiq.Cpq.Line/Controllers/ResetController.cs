092d0f5feb25a8fc8d63b0c84becb9dc0402bd2a
Pragathi963 <50317105+Pragathi963@users.noreply.github.com>
2019-11-28 19:29:46 +0530

    line grid
    changes

---
+++ Config.CPQ.LineGrid/Confiq.Cpq.Line/Controllers/ResetController.cs
@@ -0,0 +1,92 @@
+﻿using Confiq.Cpq.Line.Models;
+using Confiq.Cpq.Line.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Confiq.Cpq.Line.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ResetController : ControllerBase
+    {
+        private readonly LineService _lineService;
+
+        public ResetController(LineService lineService)
+        {
+            _lineService = lineService;
+        }
+
+        [HttpGet]
+
+        public ActionResult<List<Lines>> Get() =>
+            _lineService.Get();
+
+        [HttpGet]
+        [Route("{quoteNumber}")]
+
+        public ActionResult<List<Lines>> Get(string quotenumber)
+        {
+            _lineService.UpdateA(quotenumber);
+
+            //if (line == null)
+            //{
+            //    return NotFound();
+            //}
+            //return line;
+
+           return _lineService.Get();
+        }
+
+        //[HttpPost]
+        //public ActionResult<Lines> Create(Lines line)
+        //{
+        //    _lineService.Create(line);
+
+        //    return CreatedAtRoute("GetLine", new { id = line.Id.ToString() }, line);
+        //}
+
+        //[HttpPost]
+        //[Route("{quoteNumber}")]
+        //public ActionResult<Lines> Update(Lines line)
+        //{
+
+        //    _lineService.UpdateA(line.quoteNumber, line);
+        //    return line;
+        //}
+
+        //[HttpPost]
+        //[Route("{Id}")]
+
+        //public ActionResult<Lines> UpdateMany(Lines line)
+        //{
+        //    var updmanyresult = await collection.UpdateManyAsync(
+        //Lines<BsonDocument>.Filter.Gt("MasterID", 100),
+        //                        Builders<BsonDocument>.Update.Inc("MasterID", 10))
+        //}
+
+
+        //[HttpDelete]
+        //[Route("{Id}")]
+        //public IActionResult Delete(string id)
+        //{
+        //    var line = _lineService.Get(id);
+
+        //    if (line == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    _lineService.Remove(line);
+
+        //    return NoContent();
+        //}
+
+    }
+}
+
+
+
+
---
+++ Config.CPQ.LineGrid/Confiq.Cpq.Line/Models/FinalClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Confiq.Cpq.Line.Models
+{
+    public class FinalClass
+    {
+        public Header colDef { get; set; }
+        public IList<Lines> rowDta { get; set; }
+    }
+}
