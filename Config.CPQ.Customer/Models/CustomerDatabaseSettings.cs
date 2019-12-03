0c35963cda738a14070fd5cacea836bfb0e4267b
ChandanaG <57387834+chandana2297@users.noreply.github.com>
2019-12-03 17:28:15 +0530

    Revert "Merge branch 'dev-module' of https://github.com/NikoPaiyu/ConfigBackEnd into dev-module"
    This reverts commit 494775ce7f59995249e385a0d12c87e3efdcdaca, reversing
    changes made to 55d6795e608042426317dc9c3a47258c149a1310.

--- Config.CPQ.Customer/Models/CustomerDatabaseSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Config.CPQ.Models
-{
-    public class CustomerDatabaseSettings: ICustomerDatabaseSettings
-    {
-        public string CustomerCollectionName { get;set; }
-        public string ConnectionString { get; set; }
-        public string DatabaseName { get; set; }
-    }
-}
