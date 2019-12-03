ae5b05e30e4417a8a6c77af26b9dc5411bb41baa
ChandanaG <57387834+chandana2297@users.noreply.github.com>
2019-12-03 18:40:19 +0530

    configpage1

---
+++ Config.CPQ.ConfigPage/Controllers/ConfigController.cs
@@ -0,0 +1,43 @@
+﻿//To get all the data in ConfigPageDb database
+//URL: https://localhost:44392/api/config
+
+
+using Config.CPQ.ConfigPage.Models;
+using Config.CPQ.ConfigPage.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
+
+namespace Config.CPQ.ConfigPage.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConfigController : ControllerBase
+    {
+        private readonly ConfigService _configService;
+
+        public ConfigController(ConfigService configService)
+        {
+            _configService = configService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Configs>> Get() =>
+            _configService.Get();
+
+        [HttpGet]
+        [Route("{Id}")]
+        public ActionResult<Configs> Get(string id)
+        {
+            var config = _configService.Get(id);
+
+            if (config == null)
+            {
+                return NotFound();
+            }
+
+            return config;
+        }
+}
+        }
---
+++ Config.CPQ.ConfigPage/Services/ConfigService.cs
@@ -0,0 +1,45 @@
+﻿using Config.CPQ.ConfigPage.Models;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Config.CPQ.ConfigPage.Services
+{
+    public class ConfigService
+    {
+        private readonly IMongoCollection<Configs> _config;
+
+        public ConfigService(IConfigDatabaseSettings settings)
+        {
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+
+            _config = database.GetCollection<Configs>(settings.ConfigCollectionName);
+        }
+
+        public List<Configs> Get() =>
+            _config.Find(config => true).ToList();
+
+        public Configs Get(string id) =>
+            _config.Find<Configs>(config => config.Id == id).FirstOrDefault();
+
+        public List<Configs> Geta(string configid) =>
+            _config.Find<Configs>(config => config.configId == configid).ToList();
+
+        public List<Configs> Getb(string configname) =>
+              _config.Find<Configs>(config => config.configName == configname).ToList();
+
+        public List<Configs> Getc(string createdbyemail) =>
+            _config.Find<Configs>(config => config.createdByEmail == createdbyemail).ToList();
+
+        public List<Configs> Getd(string customerid) =>
+            _config.Find<Configs>(config => config.customerId == customerid).ToList();
+
+        public List<Configs> Gete(string category) =>
+            _config.Find<Configs>(config => config.category == category).ToList();
+
+        public List<Configs> GetConfigList(string searchText)=>
+            _config.Find<Configs>(config => config.configId == searchText|| config.configName == searchText|| config.createdByEmail == searchText|| config.customerId == searchText|| config.category == searchText).ToList();
+    
+    }
+}
